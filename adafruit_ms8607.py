e7c919696b518f3fc3be3450f02b42fab6ff4fd9
siddacious <nospam187+github@gmail.com>
2020-07-29 15:06:57 -0700

    humidity working

--- adafruit_ms8607.py
+++ adafruit_ms8607.py
@@ -16,20 +16,20 @@ Implementation Notes
 
 **Hardware:**
 
-* `Adafruit AS7341 Breakout <https://www.adafruit.com/products/45XX>`_
+* `Adafruit AS7341 Breakout <https:#www.adafruit.com/products/45XX>`_
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
-  https://github.com/adafruit/circuitpython/releases
+  https:#github.com/adafruit/circuitpython/releases
 
- * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
- * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+ * Adafruit's Bus Device library: https:#github.com/adafruit/Adafruit_CircuitPython_BusDevice
+ * Adafruit's Register library: https:#github.com/adafruit/Adafruit_CircuitPython_Register
 
 """
 
 __version__ = "0.0.0-auto.0"
-__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MS8607.git"
+__repo__ = "https:#github.com/adafruit/Adafruit_CircuitPython_MS8607.git"
 
 
 from struct import unpack_from
@@ -37,7 +37,7 @@ from time import sleep
 from micropython import const
 import adafruit_bus_device.i2c_device as i2c_device
 
-# // HSENSOR device commands
+# # HSENSOR device commands
 _MS8607_RESET_COMMAND = const(0xFE)  #
 _MS8607_READ_HUMIDITY_W_HOLD_COMMAND = const(0xE5)  #
 _MS8607_READ_HUMIDITY_WO_HOLD_COMMAND = const(0xF5)  #
@@ -47,6 +47,8 @@ _MS8607_WRITE_USER_REG_COMMAND = const(0xE6)  #
 _MS8607_READ_USER_REG_COMMAND = const(0xE7)  #
 
 _MS8607_HSENSOR_ADDR = const(0x40)  #
+_MS8607_COEFF_MUL = const(125)  #
+_MS8607_COEFF_ADD = const(-6)  #
 
 # enum MS8607_humidity_resolution
 # {
@@ -54,7 +56,7 @@ _MS8607_HSENSOR_ADDR = const(0x40)  #
 #        MS8607_humidity_resolution_8b,
 #        MS8607_humidity_resolution_10b,
 #        MS8607_humidity_resolution_11b
-# };]
+# }]
 
 
 class MS8607Humidity:
@@ -81,50 +83,67 @@ class MS8607Humidity:
         self._buffer[0] = _MS8607_READ_HUMIDITY_WO_HOLD_COMMAND
         with self.i2c_device as i2c:
             i2c.write(self._buffer, end=1)
-        sleep(0.1)  # _i2cPort->requestFrom((uint8_t)MS8607_HSENSOR_ADDR, 3U);
+        sleep(0.1)  # _i2cPort->requestFrom((uint8_t)MS8607_HSENSOR_ADDR, 3U)
 
         with self.i2c_device as i2c:
             i2c.readinto(self._buffer, end=3)
 
-        # _adc = (buffer[0] << 8) | buffer[1];
-        # crc = buffer[2];
-        raw_humidity = unpack_from(">H", self._buffer)
+        # _adc = (buffer[0] << 8) | buffer[1]
+        # crc = buffer[2]
+        raw_humidity = unpack_from(">H", self._buffer)[0]
+        crc_value = unpack_from(">B", self._buffer, offset=2)[0]
+        humidity = (raw_humidity * (_MS8607_COEFF_MUL / (1 << 16))) + _MS8607_COEFF_ADD
+        if not self._check_humidity_crc(raw_humidity, crc_value):
+            raise RuntimeError("CRC Error reading humidity data")
+        return humidity
 
-        #  *humidity =
-        #  adc * HUMIDITY_COEFF_MUL / (1UL << 16) + HUMIDITY_COEFF_ADD;
-        return raw_humidity
-        # // compute CRC
-        # uint8_t crc;
-        # status = hsensor_crc_check(_adc, crc);
-        # if (status != MS8607_status_ok)
-        #   return status;
+        # *adc = _adc
 
-        # *adc = _adc;
+        # return status
 
-        # return status;
+    @staticmethod
+    def _check_humidity_crc(value, crc):
+        polynom = 0x988000  # x^8 + x^5 + x^4 + 1
+        msb = 0x800000
+        mask = 0xFF8000
+        result = value << 8  # Pad with zeros as specified in spec
+
+        while msb != 0x80:
+            # Check if msb of current value is 1 and apply XOR mask
+            if result & msb:
+                result = ((result ^ polynom) & mask) | (result & ~mask)
+
+            # Shift by one
+            msb >>= 1
+            mask >>= 1
+            polynom >>= 1
+
+        if result == crc:
+            return True
+        return False
 
 
 # read_temperature_pressure_humidity(float *t, float *p,
 
-# set_humidity_resolution(enum MS8607_humidity_resolution res);
-# void set_humidity_i2c_master_mode(enum MS8607_humidity_i2c_master_mode mode);
+# set_humidity_resolution(enum MS8607_humidity_resolution res)
+# void set_humidity_i2c_master_mode(enum MS8607_humidity_i2c_master_mode mode)
 
-# get_compensated_humidity(float temperature, float relative_humidity, float *compensated_humidity);
+# get_compensated_humidity(float temperature, float relative_humidity, float *compensated_humidity)
 
 # /*
 # private:
-# bool hsensor_is_connected(void);
-# hsensor_reset(void);
-# hsensor_crc_check(uint16_t value, uint8_t crc);
-# hsensor_read_user_register(uint8_t *value);
-# hsensor_write_user_register(uint8_t value);
-# enum MS8607_humidity_i2c_master_mode hsensor_i2c_master_mode;
-# hsensor_humidity_conversion_and_read_adc(uint16_t *adc);
-# hsensor_read_relative_humidity(float *humidity);
-# int err = set_humidity_resolution(MS8607_humidity_resolution_12b); // 12 bits
-# hsensor_conversion_time = HSENSOR_CONVERSION_TIME_12b;
-# hsensor_i2c_master_mode = MS8607_i2c_no_hold;
-# float humidity = getHumidity();
-# float temperature = getTemperature();
-# float compensated_RH= get_compensated_humidity(temperature, humidity, &compensated_RH);
-# float dew_point = get_dew_point(temperature, humidity, &dew_point);
+# bool hsensor_is_connected(void)
+# hsensor_reset(void)
+# hsensor_crc_check(uint16_t value, uint8_t crc)
+# hsensor_read_user_register(uint8_t *value)
+# hsensor_write_user_register(uint8_t value)
+# enum MS8607_humidity_i2c_master_mode hsensor_i2c_master_mode
+# hsensor_humidity_conversion_and_read_adc(uint16_t *adc)
+# hsensor_read_relative_humidity(float *humidity)
+# int err = set_humidity_resolution(MS8607_humidity_resolution_12b) # 12 bits
+# hsensor_conversion_time = HSENSOR_CONVERSION_TIME_12b
+# hsensor_i2c_master_mode = MS8607_i2c_no_hold
+# float humidity = getHumidity()
+# float temperature = getTemperature()
+# float compensated_RH= get_compensated_humidity(temperature, humidity, &compensated_RH)
+# float dew_point = get_dew_point(temperature, humidity, &dew_point)
